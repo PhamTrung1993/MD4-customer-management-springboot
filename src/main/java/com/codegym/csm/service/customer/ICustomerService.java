2514c61345554f4e3cf2db2c90d6c32a7bc62ae3
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-11-03 22:54:16 +0700

    customer management springboot

---
+++ src/main/java/com/codegym/csm/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.codegym.csm.service.customer;
+
+import com.codegym.csm.model.Customer;
+import com.codegym.csm.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
